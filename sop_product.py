f3be32f2f9481552cc29cc62b8fa335854fc4158
Rajkumar A <108822128+AS-RK@users.noreply.github.com>
2024-07-31 17:56:11 +0530

    Update sop_product.py

--- sop_product.py
+++ sop_product.py
@@ -518,7 +518,7 @@ def evaluator(client):
     
         # if st.button("Previous"):
             
-        col1, col2, col3 = st.columns([1, 4, 0])
+        col1, col2, col3 = st.columns([1, 5, 1])
         with col1:
             if st.button("Previous"):
                 navigate_to_step(2)
