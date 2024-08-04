f5f0ae7a4edc6b662e473917bd7cde5ca91f33a1
Rajkumar A <108822128+AS-RK@users.noreply.github.com>
2024-08-04 15:19:02 +0530

    Update sop_product.py

--- sop_product.py
+++ sop_product.py
@@ -2,8 +2,8 @@ import streamlit as st
 from groq import Groq
 import os
 import base64
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+# from google.oauth2.credentials import Credentials
+# from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from datetime import datetime, timedelta
