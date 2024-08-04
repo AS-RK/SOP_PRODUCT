66c04f7ad0fec3dff6f0a1b96b61feaa490466ed
Rajkumar A <108822128+AS-RK@users.noreply.github.com>
2024-08-04 18:16:47 +0530

    Update sop_product.py

--- sop_product.py
+++ sop_product.py
@@ -733,8 +733,8 @@ def evaluator(client):
                     st.session_state.evaluation_count = st.session_state.evaluation_count - 1
                     st.error(f"An error occurred: {e}")
                 data = sheet.get_all_values()
-                st.write(data)
-                if data:
+                # st.write(data)
+                if len(data) > 0:
                     data_to_append = df.values.tolist()
                 else:
                     data_to_append = [df.columns.tolist()] + df.values.tolist()
