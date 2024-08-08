a3504bf052846fbc8505c0d41b28eaee68ea16f8
Rajkumar A <108822128+AS-RK@users.noreply.github.com>
2024-08-08 11:05:10 +0530

    Update sop_product.py

--- sop_product.py
+++ sop_product.py
@@ -783,7 +783,8 @@ def evaluator(client):
             subject_start = suggested_alternatives_text.find("Subject:**")
             subject_end = suggested_alternatives_text.find("\n\n", subject_start)
             st.session_state.subject = suggested_alternatives_text[subject_start + len("Subject:**"):subject_end].strip()
-            content_start = subject_end + 2
+            content_start = suggested_alternatives_text.find("content:**")
+            # content_start = subject_end + 2
             st.session_state.content = suggested_alternatives_text[content_start:].strip()
     
             st.title("Suggested Alternatives")
