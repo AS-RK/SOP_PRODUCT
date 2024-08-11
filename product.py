ca52035bba6ad34f16848ca79fa192dd8aa14415
Rajkumar A <108822128+AS-RK@users.noreply.github.com>
2024-08-11 16:20:07 +0530

    Update product.py

--- product.py
+++ product.py
@@ -738,6 +738,7 @@ def evaluator(client):
                     criteria_marks_dict = dict(zip(criteria, marks))
                     st.write(data[0])
                     reason = ' '.join(df['Reason'].values)
+                    reason = str(reason)
                     st.write(len(data))
                     batch_data = []
                     if data and len(data) > 1:
